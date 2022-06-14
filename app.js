b0a6598fa96a62711f904e5b8efb596714fb9f5b
William Barnes <wbarnes@slack-corp.com>
2022-06-14 08:44:48 -0700

    larger image file

--- app.js
+++ app.js
@@ -466,7 +466,7 @@ app.action('open-modal-button', async ({ ack, body, client, logger }) => {
           },
           {
             "type": "image",
-            "image_url": "https://files.slack.com/files-pri/T1DD3JH3K-F03K5UJMXFH/128x128.jpg?pub_secret=081abbb18e",
+            "image_url": "https://files.slack.com/files-pri/T1DD3JH3K-F03KE320EK0/pexels-christian-heitz-842711.jpg?pub_secret=9eb84202ef",
             "alt_text": "inspiration"
           }
         ]
