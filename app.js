b3cdd78b8a706100d75e98bb73237425bc901181
Jessica Barnes <wbarnes@slack-corp.com>
2022-06-21 08:52:19 -0700

    Updated logging for message ts

--- app.js
+++ app.js
@@ -527,7 +527,7 @@ async function noParentMessages({ message, next }) {
 }
 
 app.message(noParentMessages, async ({message, client, logger}) => {
-  console.log('Message reply received' + '\nmessage: ' + message.subtype);
+  console.log('Message reply received' + '\nmessage_ts: ' + message.ts + '\nmessage_thread_ts: ' + message.thread_ts);
   if (message.ts !== message.thread_ts){
     try {
       // Call chat.scheduleMessage with the built-in client
