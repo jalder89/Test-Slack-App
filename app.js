8e3065537863ef5b38e2dd6b2fd88db7141b5be0
Jessica Barnes <wbarnes@slack-corp.com>
2022-07-20 09:01:48 -0700

    Extended regex to include exclamation

--- app.js
+++ app.js
@@ -773,7 +773,7 @@ const ws2 = new WorkflowStep('user_test', {
 app.step(ws2);
 //#endregion
 
-app.message(/^(Jess|jess|Jessica|Jess).*/, async ({ context, say }) => {
+app.message(/^(Jess|jess|Jessica|Jess|Jess!|jess!|Jessica!|Jess!).*/, async ({ context, say }) => {
   // RegExp matches are inside of context.matches
   const greeting = context.matches[0];
 
